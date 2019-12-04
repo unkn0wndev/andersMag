bc0ae8e6ea51c9246d0e76668f7293d2519bd3f0
Tim van Osch <timvanosch2001@gmail.com>
2019-12-04 22:49:00 +0100

    added animations

---
+++ greensock.js
@@ -0,0 +1,33 @@
+const letterA = document.querySelector('#_aAnders_');
+const letterN = document.querySelector('#_n_');
+const letterD = document.querySelector('#_d_');
+const letterE = document.querySelector('#_e_');
+const letterR = document.querySelector('#_r_');
+const letterS = document.querySelector('#_s_');
+// const heart = document.querySelector('#_aAnders_');
+
+const alleLetters = [letterA, letterN, letterD, letterE, letterR, letterS];
+
+
+const alleTimeLine = new TimelineMax({});
+
+const tlm = new TimelineMax({});
+
+// TweenMax.to(letterA, 2, {x:100});
+letterA.addEventListener('click', _ =>{
+  tlm.to(letterA, 1, {scaleX: 7});
+  tlm.to(letterA, 1, {scaleY: 7});
+  tlm.to(letterA, 1, {scaleZ: 7});
+  tlm.to(letterA, 1, {scaleX: 1});
+  tlm.to(letterA, 1, {scaleY: 1});
+  tlm.to(letterA, 1, {scaleZ: 1});
+})
+
+letterD.addEventListener('click', _ =>{
+alleTimeLine.to(alleLetters, 1, {scaleX:10});
+alleTimeLine.to(alleLetters, 1, {scaleY:-10});
+alleTimeLine.to(alleLetters, 1, {scaleZ:10});
+alleTimeLine.to(alleLetters, 2, {scaleX:1});
+alleTimeLine.to(alleLetters, 2, {scaleY:1});
+alleTimeLine.to(alleLetters, 2, {scaleZ:1});
+})
