aae303007e1c08acf5c2750c07eb264a08f318ce
Tim van Osch <timvanosch2001@gmail.com>
2020-01-30 12:13:29 +0100

    Added smooth heart animation (automatic)

--- greensock.js
+++ greensock.js
@@ -6,7 +6,7 @@ const letterR = document.querySelector('#_r_');
 const letterS = document.querySelector('#_s_');
 const ma = document.querySelector('#_maRechthoek_');
 const hart = document.querySelector('#_hart_');
-const hart = document.querySelector('#_hart_');
+const vierkant = document.querySelector('#_vierkant_');
 
 const alleLetters = [letterA, letterN, letterD, letterE, letterR, letterS];
 
@@ -19,41 +19,87 @@ const malijn = new TimelineMax({});
 
 const hartlijn = new TimelineMax({});
 
-hart.addEventListener('click', _ =>{
-  hartlijn.to(hart, 10,
-    {
+const vierkantlijn = new TimelineMax({});
+
+
+vierkantlijn.to(hart, 10, {
+  rotation: 360,
+  transformOrigin: "center",
+  scale: 3,
+  ease: Power1.easeOut,
+  repeat: -1,
+  ease: Power1.easeIn,
+  repeat: -1,
+}, 0);
+
+hart.addEventListener('click', _ => {
+  hartlijn.to(hart, 10, {
       rotation: 360,
-      transformOrigin:"center",
-      scale:1.5,
-      ease: Power0.easeNone,
-      repeat:-1
+      transformOrigin: "center",
+      scale: 1.5,
+      ease: Power1.easeNone,
+      repeat: -1
     },
-  0);
+    0);
 })
 
 
 
-ma.addEventListener('click', _ =>{
-malijn.to(ma, 1, {scaleX: 5});
-malijn.to(ma, 1, {scaleY: 5});
+ma.addEventListener('click', _ => {
+  malijn.to(ma, 1, {
+    scaleX: -50
+  });
+  malijn.to(ma, 1, {
+    scaleY: -50
+  });
+  malijn.to(ma, 1, {
+    scaleX: 0
+  });
+  malijn.to(ma, 1, {
+    scaleY: 0
+  });
 })
 
 
 // TweenMax.to(letterA, 2, {x:100});
-letterA.addEventListener('click', _ =>{
-  tlm.to(letterA, 1, {scaleX: 7});
-  tlm.to(letterA, 1, {scaleY: 7});
-  tlm.to(letterA, 1, {scaleZ: 7});
-  tlm.to(letterA, 1, {scaleX: 1});
-  tlm.to(letterA, 1, {scaleY: 1});
-  tlm.to(letterA, 1, {scaleZ: 1});
+letterA.addEventListener('click', _ => {
+  tlm.to(letterA, 1, {
+    scaleX: 7
+  });
+  tlm.to(letterA, 1, {
+    scaleY: 7
+  });
+  tlm.to(letterA, 1, {
+    scaleZ: 7
+  });
+  tlm.to(letterA, 1, {
+    scaleX: 1
+  });
+  tlm.to(letterA, 1, {
+    scaleY: 1
+  });
+  tlm.to(letterA, 1, {
+    scaleZ: 1
+  });
 })
 
-letterD.addEventListener('click', _ =>{
-alleTimeLine.to(alleLetters, 1, {scaleX:10});
-alleTimeLine.to(alleLetters, 1, {scaleY:-10});
-alleTimeLine.to(alleLetters, 1, {scaleZ:10});
-alleTimeLine.to(alleLetters, 2, {scaleX:1});
-alleTimeLine.to(alleLetters, 2, {scaleY:1});
-alleTimeLine.to(alleLetters, 2, {scaleZ:1});
+letterD.addEventListener('click', _ => {
+  alleTimeLine.to(alleLetters, 1, {
+    scaleX: 10
+  });
+  alleTimeLine.to(alleLetters, 1, {
+    scaleY: -10
+  });
+  alleTimeLine.to(alleLetters, 1, {
+    scaleZ: 10
+  });
+  alleTimeLine.to(alleLetters, 2, {
+    scaleX: 1
+  });
+  alleTimeLine.to(alleLetters, 2, {
+    scaleY: 1
+  });
+  alleTimeLine.to(alleLetters, 2, {
+    scaleZ: 1
+  });
 })
